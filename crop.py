d81f6f33150edf71e6fd0fcf60a32ddd1458cc92
Ameya Kunder <a.kunder@berkeley.edu>
2020-09-28 00:40:52 -0700

    Ask user to crop when auto not good enough

--- crop.py
+++ crop.py
@@ -3,7 +3,10 @@
 import argparse
 import numpy as np
 import cv2
+import glob
+import imutils
 
+ref_point = []
 
 class PBv3Matcher:
     def __init__(self, golden):
@@ -53,8 +56,8 @@ class PBv3Matcher:
         # store all the good matches as per Lowe's ratio test
         good = []
         for m in matches:
-            if len(m)==1: continue
-            if m[0].distance < 0.7*m[1].distance:
+            if len(m)<=1: continue
+            elif m[0].distance < 0.7*m[1].distance:
                 good.append(m[0])
 
         if len(good)<self.MIN_MATCH_COUNT:
@@ -72,24 +75,76 @@ class PBv3Matcher:
 
         return timg
 
+    def shape_select(event, x, y, flags, param):
+        global ref_point
+
+        if event == cv2.EVENT_LBUTTONDOWN:
+            ref_point = [(x, y)]
+
+        elif event == cv2.EVENT_LBUTTONUP:
+            ref_point.append((x, y))
+
+        cv2.rectangle(iimg, ref_point[0], ref_point[1], (0, 0, 0), 2)
+        cv2.imshow("image", iimg)
+
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Crop a sheildbox from a picture of a Powerboard')
-    parser.add_argument('input' ,help='Path to image of a Powerboard')
-    parser.add_argument('output',help='Path where the result will be saved')
+    parser.add_argument('input' ,help='Path to images of Powerboards')
+    parser.add_argument('output',help='Path where the results will be saved')
 
     args = parser.parse_args()
 
-    iimg = cv2.imread(args.input)
+    for x in glob.glob(f"{args.input}/*JPG"):
 
-    cv2.imshow("test",iimg)
-    cv2.imwrite(args.output, iimg)
-    cv2.waitKey(0)
-    
-    trf=PBv3Matcher('data/golden.jpg')
-    timg=trf.transform(iimg)
+        iimg = cv2.imread(x)
 
-    cimg=timg[260:260+560,750:750+625]
-    cv2.imshow("test",cimg)
-    cv2.waitKey(0)
+        cv2.namedWindow('test', cv2.WINDOW_NORMAL)
+        cv2.resizeWindow('test', 900, 600)
+        cv2.imshow('test', iimg)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    cv2.imwrite(args.output, cimg)
+        serial = input("Enter the serial number: ")
+        cv2.imwrite(f"{args.output}/{serial}.JPG", iimg)
+        
+        trf=PBv3Matcher('data/golden.jpg')
+        timg=trf.transform(iimg)
+
+        if timg is None:
+            iimg = imutils.resize(iimg, width = 900)
+            #reads and resizes the image
+
+            clone = iimg.copy()
+            cv2.namedWindow("image")
+            cv2.moveWindow("image", 20, 20)
+            cv2.setMouseCallback("image", PBv3Matcher.shape_select)
+
+            while True:
+                # display the image and wait for a keypress
+                cv2.imshow("image", iimg)
+                key = cv2.waitKey(1) & 0xFF
+
+                # if the 'r' key is pressed, reset the cropping region
+                if key == ord("r"):
+                    iimg = clone.copy()
+
+                # if the 'c' key is pressed, break from the loop
+                elif key == ord("c"):
+                    break
+
+            if len(ref_point) == 2:
+                crop_img = clone[ref_point[0][1]:ref_point[1][1], ref_point[0][0]:ref_point[1][0]]
+                crop_img = cv2.resize(crop_img, (600, 500))
+                cv2.imshow("crop_img", crop_img)
+                cv2.waitKey(0)
+
+            write_add = f"{args.output}/{serial}.JPG"
+            cv2.imwrite(write_add, crop_img)
+            cv2.destroyAllWindows()
+
+        else:
+            cimg=timg[260:260+560,750:750+625]
+            #cv2.imshow("test",cimg)
+            #cv2.waitKey(0)
+            write_add = f"{args.output}/{serial}.JPG"
+            cv2.imwrite(write_add, cimg)
